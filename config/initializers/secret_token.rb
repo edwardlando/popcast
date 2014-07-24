462828600acaaae15e591e06ced74395d4f4dd6f
edwardlando <edwardlando8@gmail.com>
2014-07-24 15:42:14 -0700

    basic page:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Popcast::Application.config.secret_token = '21889c280bff2d6dec7708db7f34a633ebfa790aab65f573f3dcef3eb303e2939f8c7158932a5a5ee00e51f2d25a2a554dfae10160216c74aa12f01601cc0b5f'
